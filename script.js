6b3b4bb37023d89ee9d0d874d230a936d5f16c67
so <hiroaki.so@smarthr.co.jp>
2023-04-12 11:11:56 +0900

    Update script.js

--- script.js
+++ script.js
@@ -9,20 +9,18 @@ const output = document.getElementById('output');
 
 button_convert.addEventListener('click', () => {
     const rawText = area_input.value;
-    const rawTextWithoutHeading = rawText
-        .replace(/"url".*\n/,''); //タイトル行を消す
-    let arr1 = rawTextWithoutHeading
-        .split('\n'); // 1行ずつ配列に格納する
+    const arr1 = rawText.split('\n') // 1行ずつ配列に格納する
+    arr1.shift(); // ヘッダ行の分を削除
     arr1.sort();  // ソートする（＝URL順になる）
-    let convertedText = 'URL\tTitle\tRoot\tL2\tL3\tL4\tL5\n'; //ヘッダ行
+    let convertedText = 'URL\tTitle\tRoot\tL2\tL3\tL4\tL5\tL6\n'; //ヘッダ行
     for (let i = 0; i < arr1.length; i++ ){
-        if (arr1[i] !== ''){ //空白行以外
-        arr1[i] = arr1[i].replace(/^\"(.*)\"$/g,'$1'); //最初と最後のクォートを消す
-        let arr2 = arr1[i].split('\",\"') // "," ごとに配列化。{url, title} になる
-        arr2.splice(2, arr2.length - 2); //URLとTitle以外を削除
-        arr2.push(arr2[0]); // URLを複製して末尾に追加
-        arr2[2] = arr2[2].replace(/([^\/])\/([^(\/|\n\$)])/g,'$1\/\t$2');  //末尾の方のURLに、スラッシュごとにtab文字を挿入
-        convertedText += arr2[0]  +'\t' + arr2[1]  + '\t' + arr2[2] + '\n';  //タブ区切りテキスト化する
+        if (arr1[i] !== ''){ //空白行を無視する
+            arr1[i] = arr1[i].replace(/^\"(.*)\"$/g,'$1'); //最初と最後のクォートを消す
+            const arr2 = arr1[i].split('\",\"') // "," ごとに配列化し、[URL, Title, xxx, xxx, ...] を作る
+            arr2.splice(2, arr2.length - 2); //URLとTitle以外を削除し、[URL, Title] だけにする
+            arr2.push(arr2[0]); // URLを複製して末尾に追加
+            arr2[2] = arr2[2].replace(/([^\/])\/([^(\/|\n\$)])/g,'$1\/\t$2');  //末尾の方のURLに、スラッシュごとにtab文字を挿入
+            convertedText += arr2[0]  +'\t' + arr2[1]  + '\t' + arr2[2] + '\n';  //タブ区切りテキスト化する
         }
     }
     area_output.value = convertedText;
