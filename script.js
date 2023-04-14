25481fb38c2a4f39789bba0a8af424da41b0e6ea
so <hiroaki.so@smarthr.co.jp>
2023-04-14 09:11:16 +0900

    変数名を改善
    変数名を分かりやすいものに変更

--- script.js
+++ script.js
@@ -8,22 +8,22 @@ const button_copy = document.getElementById('button_copy');
 const output = document.getElementById('output');
 
 button_convert.addEventListener('click', () => {
-    const rawText = area_input.value;
-    const arr1 = rawText.split('\n') // 1行ずつ配列に格納する
-    arr1.shift(); // ヘッダ行の分を削除
-    arr1.sort();  // ソートする（＝URL順になる）
-    let convertedText = 'URL\tTitle\tRoot\tL2\tL3\tL4\tL5\tL6\n'; //ヘッダ行
-    for (let i = 0; i < arr1.length; i++ ){
-        if (arr1[i] !== ''){ //空白行を無視する
-            arr1[i] = arr1[i].replace(/^\"(.*)\"$/g,'$1'); //最初と最後のクォートを消す
-            const arr2 = arr1[i].split('\",\"') // "," ごとに配列化し、[URL, Title, xxx, xxx, ...] を作る
-            arr2.splice(2, arr2.length - 2); //URLとTitle以外を削除し、[URL, Title] だけにする
-            arr2.push(arr2[0]); // URLを複製して末尾に追加
-            arr2[2] = arr2[2].replace(/([^\/])\/([^(\/|\n\$)])/g,'$1\/\t$2');  //末尾の方のURLに、スラッシュごとにtab文字を挿入
-            convertedText += arr2[0]  +'\t' + arr2[1]  + '\t' + arr2[2] + '\n';  //タブ区切りテキスト化する
-        }
+    const text_input = area_input.value.trim();
+    const array_byLine = text_input.split('\n') // 1行ずつ配列に格納する
+    array_byLine.shift(); // ヘッダ行の分を削除
+    array_byLine.sort();  // ソートする（＝URL順になる）
+    let text_output = `URL\tTitle\tRoot\tL2\tL3\tL4\tL5\tL6`; //新ヘッダ行
+    for (let i = 0; i < array_byLine.length; i++ ){
+        // if (array_byLine[i] !== ''){ //空白行を無視する
+            array_byLine[i] = array_byLine[i].replace(/^\"(.*)\"$/g,'$1'); //最初と最後のクォートを消す
+            const array_byComma = array_byLine[i].split('\",\"') // "," ごとに配列化し、[URL, Title, xxx, xxx, ...] を作る
+            array_byComma.splice(2, array_byComma.length - 2); //URLとTitle以外を削除し、[URL, Title] だけにする
+            array_byComma.push(array_byComma[0]); // URLを複製して末尾に追加
+            array_byComma[2] = array_byComma[2].replace(/([^\/])\/([^(\/|\n\$)])/g,'$1\/\t$2');  //末尾の方のURLに、スラッシュごとにtab文字を挿入
+            text_output += `\n` + array_byComma[0]  +'\t' + array_byComma[1]  + '\t' + array_byComma[2] + '\n';  //タブ区切りテキスト化する
+        // }  
     }
-    area_output.value = convertedText;
+    area_output.value = text_output;
 });
 
 button_clear.addEventListener('click', () => {
@@ -32,12 +32,12 @@ button_clear.addEventListener('click', () => {
 
 button_copy.addEventListener('click', () => {
     navigator.clipboard.writeText(area_output.value);
-    const msgBox = document.createElement('div');
-    msgBox.id = 'message';
-    msgBox.textContent = 'Copied!';
-    output.appendChild(msgBox);
+    const div_message = document.createElement('div');
+    div_message.id = 'message';
+    div_message.textContent = 'Copied!';
+    output.appendChild(div_message);
     setTimeout(() => {
-        msgBox.remove();
+        div_message.remove();
     }, 1000);
 });
 
